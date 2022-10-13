c32320b15def7c267f7523fafd22b7359eca1f20
André Pires <andrebeckpires@gmail.com>
2022-10-12 22:53:12 -0300

    esboço de menu

---
+++ Assets/Scripts/changeMinigames.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeMinigames : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     public void mg1()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+       public void mg2()
+    {
+        SceneManager.LoadScene("minigame2");
+    }
+
+    public void goMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
