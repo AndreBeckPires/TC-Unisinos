e64db2a0a0f9735d33a1f6ce920f42d92829dce0
André Pires <andrebeckpires@gmail.com>
2022-11-20 21:41:02 -0300

    arrumando varios bugs

--- Assets/Scripts/selectTriangle.cs
+++ Assets/Scripts/selectTriangle.cs
@@ -22,6 +22,8 @@ public class selectTriangle : MonoBehaviour
     public bool first = true;
     public Text txt;
     int points = 0;
+    public bool neutro = false;
+    public List<GameObject> objToDestroy = new List<GameObject>();
      private  Color[] colors = {new Color(1,0,0,1), new Color(1,1,1,1)};
     // Start is called before the first frame update
     void Start()
@@ -38,12 +40,21 @@ public class selectTriangle : MonoBehaviour
             t1 =0;
             points++;
             txt.text = points.ToString();
-            Instantiate(explosion, objetos[0].transform.position, Quaternion.identity);
-            Destroy(objetos[0]);
-            Instantiate(explosion, objetos[1].transform.position, Quaternion.identity);
-            Destroy(objetos[1]);
-            Instantiate(explosion, objetos[2].transform.position, Quaternion.identity);
-            Destroy(objetos[2]);
+            for(int i = 0; i < objToDestroy.Count; i++)
+            {
+                 Instantiate(explosion, objToDestroy[i].transform.position, Quaternion.identity);
+                  Destroy(objToDestroy[i]);
+            }
+            objToDestroy.Clear();
+            st1 = false;
+            first = true;
+            neutro = false;
+         //   Instantiate(explosion, objetos[0].transform.position, Quaternion.identity);
+         //   Destroy(objetos[0]);
+          //  Instantiate(explosion, objetos[1].transform.position, Quaternion.identity);
+           // Destroy(objetos[1]);
+           // Instantiate(explosion, objetos[2].transform.position, Quaternion.identity);
+           // Destroy(objetos[2]);
         }
         
         if(t2 == 3)
@@ -51,12 +62,15 @@ public class selectTriangle : MonoBehaviour
             t2 =0;
             points++;
             txt.text = points.ToString();
-            Instantiate(explosion, objetos[0].transform.position, Quaternion.identity);
-            Destroy(objetos[0]);
-            Instantiate(explosion, objetos[1].transform.position, Quaternion.identity);
-            Destroy(objetos[1]);
-            Instantiate(explosion, objetos[2].transform.position, Quaternion.identity);
-            Destroy(objetos[2]);
+            for(int i = 0; i < objToDestroy.Count; i++)
+            {
+                 Instantiate(explosion, objToDestroy[i].transform.position, Quaternion.identity);
+                  Destroy(objToDestroy[i]);
+            }
+             objToDestroy.Clear();
+            st2 = false;
+            first = true;
+            neutro = false;
             
         }
     }
@@ -76,18 +90,23 @@ public class selectTriangle : MonoBehaviour
                     if(t2 != 0){
                         if(hit.collider.tag == "T1")
                         error();
+                        Debug.Log("1");
                     }
                     if(t1 != 0 )
                     {
                          if(hit.collider.tag == "T2")
                         error();
+                        Debug.Log("2");
                     }
                     if(hit.collider.tag == "T1")
                     {
+                        objToDestroy.Add(hit.collider.gameObject);
+                        
                         st1 = true;
                         st2 = false;
                         t1++;
                         first = false;
+                        Debug.Log("3");
                       
                     }
                     if(hit.collider.tag == "T2")
@@ -95,47 +114,137 @@ public class selectTriangle : MonoBehaviour
                         st2 = true;
                         st1 = false;
                         t2++;
+                        first = false;  
+                        Debug.Log("4");     
+                        objToDestroy.Add(hit.collider.gameObject);
+                    }
+                    if(hit.collider.name == "5x5")
+                    {
                         first = false;
-                     
-                       
+                        neutro = true;
+                        Debug.Log("5");
+                        objToDestroy.Add(hit.collider.gameObject);
                     }
                 }
                 else{
-                    if(st1)
+                  
+                    if(st1 == false && st2 == false)
+                    {
+                         if(hit.collider.tag == "T1")
+                         {
+                            st1 = true;
+                            t1 += 2;
+                            Debug.Log("6");
+                            objToDestroy.Add(hit.collider.gameObject);
+                         }
+                        
+                          if(hit.collider.tag == "T2")
+                          {
+                             st2 = true;
+                             t2 += 2;
+                             Debug.Log("7");
+                             objToDestroy.Add(hit.collider.gameObject);
+                          }
+                          if( hit.collider.name == "5x5")
+                          {
+                            error();
+                            Debug.Log("12");  
+                           
+                            st2 = false;
+                            st1 = false;
+                            t1 = 0;
+                            t2 = 0;  
+                          }                          
+                    }
+                   else if(st1)
                     {
                         if(hit.collider.tag == "T1")
                         {
+                            objToDestroy.Add(hit.collider.gameObject);
+                            st1 = true;
+                            st2= false;
                             t1++;
+                         Debug.Log("8");
+
                         }
-                        if(hit.collider.tag == "T2")
+                        if(hit.collider.tag == "T2" &&  hit.collider.name != "5x5")
                         {
                             t1 = 0;
                             t2++;
-                           // st1 = false;
-                          //  st2 = true;
-                            first = true;
+                           st1 = false;
+                           st2 = true;
+                        neutro = false;
                             error();
+                             objToDestroy.Add(hit.collider.gameObject);
+                            Debug.Log("9");
                             
                         }
-                    }
-                    if(st2)
-                    {
-                       
-                        if(hit.collider.tag == "T2")
+                        if(hit.collider.name == "5x5")
                         {
+                            if(neutro)
+                            {
+                                error();
+
+                                st1 = false;
+                                st2 = false;
+                                t1 = 0;
+                                t2 =0;
+                            }
+                            else{
+                            st1 = true;
+                            st2= false;
+                            t1++;
+                             objToDestroy.Add(hit.collider.gameObject);
+                            }
+                           
+                         Debug.Log("13");
+                        }
+                        
+                    }
+                  else  if(st2)
+                    {
+                  
+
+                        if(hit.collider.tag == "T2") 
+                        {   
+                            st1 = false;
+                            st2 = true;
                             t2++;
+                        Debug.Log("10");
+                        objToDestroy.Add(hit.collider.gameObject);
                            
                         }
-                        if(hit.collider.tag == "T1")
+                        if(hit.collider.tag == "T1"  &&  hit.collider.name != "5x5")
                         {
-                             Debug.Log("entrou");
                             //  st1 = true;
                           //  st2 = false;
                             t2 = 0;
                             t1++;
-                            first = true;
-                            
+                            neutro = false;
+                            st2 = false;
+                            st1 = true;
+                            Debug.Log("11");
                             error();
+                              objToDestroy.Add(hit.collider.gameObject);
+                        }
+                         if(hit.collider.name == "5x5")
+                        {
+                              if(neutro)
+                            {
+                                error();
+                                st1 = false;
+                                st2 = false;
+                                t1 = 1;
+                                t2 =0;
+                            }
+                            else{
+                            st1 = false;
+                            st2= true;
+                            t2++;
+                            objToDestroy.Add(hit.collider.gameObject);
+                            }
+                            
+                         Debug.Log("14");
                         }
                     }
                     
@@ -159,7 +268,8 @@ public class selectTriangle : MonoBehaviour
 
     void error()
     {
-       
+        objToDestroy.Clear();
+      // neutro = false;
         for(int i =0; i < objetos.Length; i++)
         {
              spriteRenderer = objetos[i].GetComponent<SpriteRenderer>();
