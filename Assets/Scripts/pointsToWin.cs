76d2c06a29cf0fafebef6c0e9835949f9c17d2df
André Pires <andrebeckpires@gmail.com>
2022-11-30 21:08:22 -0300

    tangram new models

--- Assets/Scripts/pointsToWin.cs
+++ Assets/Scripts/pointsToWin.cs
@@ -9,9 +9,10 @@ public class pointsToWin : MonoBehaviour
     private int currentPoints;
     public GameObject[] items;
     public GameObject canvas, loose;
-    public GameObject t1,t2,tb1,tb2;
+    public GameObject t1,t2,tb1,tb2,t3,tb3;
     public int winsCounter = 0;
     public float timer = 8;
+    public bool f1 = false,f2 = false,f3 = false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +26,11 @@ public class pointsToWin : MonoBehaviour
         timer -= Time.deltaTime;
         if(timer <= 0)
         {
-            youLoose();
+           // youLoose();
         }
-        if(winsCounter >= 2)
+        if(winsCounter >= 3)
         {
-            youLoose();
+            youWin();
         }
         if(currentPoints >= pointsToWinGame){
             //win
@@ -38,16 +39,47 @@ public class pointsToWin : MonoBehaviour
            winsCounter++;
            if(t1.activeSelf)
            {
+            f1 = true;
             t1.SetActive(false);
             tb1.SetActive(false);
-            t2.SetActive(true);
-            tb2.SetActive(true);
+            if(Random.Range(0,2) == 1 && f2 == false)
+            {
+                t2.SetActive(true);
+                tb2.SetActive(true);
+            }
+            else{
+                t3.SetActive(true);
+                tb3.SetActive(true);
+            }
+          
            }
-           else{
+            else if(t2.activeSelf){
+            f2 = true;
             t2.SetActive(false);
             tb2.SetActive(false);
-            t1.SetActive(true);
-            tb1.SetActive(true);
+             if(Random.Range(0,2) == 1 && f1 == false)
+            {
+                t1.SetActive(true);
+                tb1.SetActive(true);
+            }
+            else{
+                t3.SetActive(true);
+                tb3.SetActive(true);
+            }
+           }
+            else if(t3.activeSelf){
+            f3 = true;
+            t3.SetActive(false);
+            tb3.SetActive(false);
+            if(Random.Range(0,2) == 1 && f2 == false)
+            {
+                t2.SetActive(true);
+                tb2.SetActive(true);
+            }
+            else{
+                t1.SetActive(true);
+                tb1.SetActive(true);
+            }
            }
         }
     }
--- Assets/Scripts/spawnercolorchanger.cs
+++ Assets/Scripts/spawnercolorchanger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class spawnercolorchanger : MonoBehaviour
 {
     public GameObject[] objects;
-    public GameObject t1,t2,tb1,tb2;
+    public GameObject t1,t2,tb1,tb2,t3,tb3;
     SpriteRenderer sprite;
     int rand;
     private  Color[] colors = {new Color(1,0,0,1), new Color(0,1,0,1), new Color(0,0,1,1)};
@@ -17,16 +17,27 @@ public class spawnercolorchanger : MonoBehaviour
             sprite = objects[i].GetComponent<SpriteRenderer>();
             sprite.color = colors[Random.Range(0, 3)];
         }
-        rand = Random.Range(0,100);
-        if(rand%2 == 0)
+        rand = Random.Range(0,12);
+        if(rand == 0 || rand == 2 || rand == 5 || rand == 8 || rand == 11)
         {
             Debug.Log("entrou");
             t1.SetActive(false);
             tb1.SetActive(false);
+            t3.SetActive(false);
+            tb3.SetActive(false);
         }
-        else{
+        else if(rand == 1 || rand == 3 || rand == 6 || rand == 9 || rand == 12){
             Debug.Log("entrou");
             t2.SetActive(false);
+            tb2.SetActive(false);
+            t1.SetActive(false);
+            tb1.SetActive(false);
+        }
+        else if(rand == 4 || rand == 7 || rand == 10 || rand == 12){
+            Debug.Log("entrou");
+            t3.SetActive(false);
+            tb3.SetActive(false);
+              t2.SetActive(false);
             tb2.SetActive(false);
         }
     }
