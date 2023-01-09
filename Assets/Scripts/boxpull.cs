554a0f3f9432887ae01c803d9d2c1bcdd8e663ee
André Pires <andrebeckpires@gmail.com>
2023-01-08 22:55:32 -0300

    os objetos ficam no lugar correto

--- Assets/Scripts/boxpull.cs
+++ Assets/Scripts/boxpull.cs
@@ -6,11 +6,15 @@ public class boxpull : MonoBehaviour
 {
     public bool beingPushed;
     float xPos,yPos;
+    public GameObject goal;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = this.GetComponent<Rigidbody2D>();
         xPos = transform.position.x;   
         yPos = transform.position.y; 
+        
     }
 
     // Update is called once per frame
@@ -23,6 +27,16 @@ public class boxpull : MonoBehaviour
         else
         {
             xPos = transform.position.x;
+            yPos = transform.position.y;
         }
+        if(this.transform.position.x - goal.transform.position.x < 0.05f && this.transform.position.y - goal.transform.position.y < 0.05f)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            this.GetComponent<FixedJoint2D>().enabled = false;
+           this.gameObject.tag = "T1";
+           this.transform.position = goal.transform.position;
+            Destroy(goal);
+           Debug.Log("nolugar");
+         }
     }
 }
